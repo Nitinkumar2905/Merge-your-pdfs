f0dcfe93c7c46742b1e03e308c24f130f9cf5b2b
Nitin <nimble2905@gmail.com>
2023-01-15 16:34:59 +0530

    some changees

--- merge.js
+++ merge.js
@@ -3,8 +3,8 @@ const PDFMerger = require('pdf-merger-js');
 var merger = new PDFMerger();
 
 const mergepdfs = async (p1,p2)=>{
-    await merger.add('1.pdf')
-    await merger.add('2.pdf')
+    await merger.add(p1);
+    await merger.add(p2);
 
 
     await merger.save('public/merged.pdf');
